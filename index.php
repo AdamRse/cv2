48af456afc00211041db6ff70dd65b0c35fc962e
AdamRse <bakaarion3@gmail.com>
2024-12-18 22:56:57 +0100

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV Adam Rousselle</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="page">
+        <div id="partLeft" class="paddingTop">
+            <div class="title">
+                <div class="decorationBlock"></div>
+                <div class="text">Développeur web</div>
+            </div>
+        </div>
+        <div id="partRight" class="paddingTop">
+sdfsdf
+        </div>
+    </div>
+</body>
+</html>
